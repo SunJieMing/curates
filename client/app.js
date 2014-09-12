2d5283bd997b160fa1def240ea817f37dbfd4f78
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-09-11 20:10:10 -0700

    debug commits

--- client/app.js
+++ client/app.js
@@ -12,7 +12,6 @@ angular.module('curates', [
 ])
 
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-  // $urlRouterProvider.otherwise('/');
   $stateProvider
   	.state('/signin', {
   		url: '/signin',
