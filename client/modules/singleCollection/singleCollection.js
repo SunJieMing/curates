e0cbd926084ad56d41b68a5364a035a81e8e5a71
bmcnary <bmcnary@live.com>
2014-09-11 20:06:52 -0700

    fixed issues from rebase

--- client/modules/singleCollection/singleCollection.js
+++ client/modules/singleCollection/singleCollection.js
@@ -2,10 +2,10 @@ angular.module('curates.singleCollection', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('singleCollection', {
-      url: '/collections/:url',
-      templateUrl: 'modules/singleCollection/singleCollection.html'
-    })
+  .state('singleCollection', {
+    url: '/collections/:url',
+    templateUrl: 'modules/singleCollection/singleCollection.html'
+  })
 })
 
 .controller('singleCollectionController', function($scope, $state, $stateParams, collectionFactory, Auth) {
@@ -18,15 +18,37 @@ angular.module('curates.singleCollection', [])
     if (collection != null) {
       $scope.isUser = Auth.isAuth().data === "OK";
          // Auth.user.provider === collection.user.provider
-      $scope.collection = collection;
-    }
-  });
+         $scope.collection = collection;
+       }
+     });
 
   $scope.upVote = function() {
     $scope.collection.stars++;
     collectionFactory.updateCollection($scope.collection);
     $scope.notYetUpvoted = false;
   };
+
+  $scope.downVote = function() {
+    $scope.collection.stars--;
+    collectionFactory.updateCollection($scope.collection);
+    $scope.notYetUpvoted = false;
+  };
+
+  $scope.upVoteLink = function(index) {
+    $scope.collection.links[index].stars++;
+    console.log($scope.collection.links);
+    collectionFactory.updateCollection($scope.collection);
+    $scope.collection.links[index].voted = true;
+  };
+
+  $scope.downVoteLink = function(index) {
+    $scope.collection.links[index].stars--;
+    console.log($scope.collection.links);
+    collectionFactory.updateCollection($scope.collection);
+    $scope.collection.links[index].voted = true;
+  };
+
+
 
   $scope.clone = function() {
     $state.go('createCollection', {
